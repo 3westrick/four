3f94186224e3857a93a708ed9fc1e159bb4a548f
Arad Afraz <threewestrick@gmail.com>
2025-05-07 10:07:05 +0330

    modify middleware

--- src/app/(client)/layout.tsx
+++ src/app/(client)/layout.tsx
@@ -1,33 +1,42 @@
+import { getUser, signOut } from "@/utils/supabase/auth";
 import Link from "next/link";
 
 export default async function layout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <>
-            <header>
-                <nav className="flex justify-between py-2 px-4 border-b border-b-stone-700 border-dashed">
-                    <ul className="flex gap-2">
-                        <li>
-                            <Link href={"/"}>Home</Link>
-                        </li>
-                        <li>
-                            <Link href={"/todos"}>Todos</Link>
-                        </li>
-                    </ul>
-                    <ul className="flex gap-2">
-                        <li>
-                            <Link href={"/login"}>Login</Link>
-                        </li>
-                        <li>
-                            <button className="cursor-pointer">Logout</button>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main className="p-4">{children}</main>
-        </>
-    );
+  const user = await getUser();
+  return (
+    <>
+      <header>
+        <nav className="flex justify-between border-b border-dashed border-b-stone-700 px-4 py-2">
+          <ul className="flex gap-2">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href={"/todos"}>Todos</Link>
+            </li>
+          </ul>
+          <ul className="flex gap-2">
+            {!user ? (
+              <li>
+                <Link href={"/login"}>Login</Link>
+              </li>
+            ) : (
+              <li>
+                <form>
+                  <button className="cursor-pointer" formAction={signOut}>
+                    Logout
+                  </button>
+                </form>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </header>
+      <main className="p-4">{children}</main>
+    </>
+  );
 }
--- src/utils/supabase/auth.ts
+++ src/utils/supabase/auth.ts
@@ -5,72 +5,80 @@ import { createClient } from "./server";
 import { revalidatePath } from "next/cache";
 
 export async function emailLogin(formData: FormData) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
-    const data = {
-        email: formData.get("email") as string,
-        password: formData.get("password") as string,
-    };
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-    const { error } = await supabase.auth.signInWithPassword(data);
+  const { error } = await supabase.auth.signInWithPassword(data);
 
-    if (error) {
-        redirect("/error");
-    }
+  if (error) {
+    redirect("/error");
+  }
 
-    revalidatePath("/", "layout");
-    redirect("/todos");
+  revalidatePath("/", "layout");
+  redirect("/todos");
 }
 
 export async function signup(formData: FormData) {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    // type-casting here for convenience
-    // in practice, you should validate your inputs
-    const data = {
-        email: formData.get("email") as string,
-        password: formData.get("password") as string,
-    };
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-    const { error } = await supabase.auth.signUp(data);
+  const { error } = await supabase.auth.signUp(data);
 
-    if (error) {
-        redirect("/error");
-    }
+  if (error) {
+    redirect("/error");
+  }
 
-    revalidatePath("/", "layout");
-    redirect("/login");
+  revalidatePath("/", "layout");
+  redirect("/login");
 }
 
 export async function googleAuth() {
-    const supabase = await createClient();
+  const supabase = await createClient();
 
-    const { error } = await supabase.auth.signInWithOAuth({
-        provider: "google",
-    });
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+  });
 
-    if (error) {
-        redirect("/error");
-    }
+  if (error) {
+    redirect("/error");
+  }
 
-    revalidatePath("/", "layout");
-    redirect("/");
+  revalidatePath("/", "layout");
+  redirect("/");
 }
 
 export async function signOut() {
-    const supabase = await createClient();
-    await supabase.auth.signOut();
-    revalidatePath("/", "layout");
-    redirect("/login");
+  const supabase = await createClient();
+  await supabase.auth.signOut();
+  revalidatePath("/", "layout");
+  redirect("/login");
 }
 
 export async function isAuthenticated() {
-    const supabase = await createClient();
-    const { data, error } = await supabase.auth.getUser();
-    if (error || !data?.user) {
-        redirect("/login");
-    }
-    return data.user;
+  const user = await getUser();
+  if (!user) {
+    redirect("/login");
+  }
+  return user;
+}
+
+export async function getUser() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    return null;
+  }
+  return data.user;
 }
